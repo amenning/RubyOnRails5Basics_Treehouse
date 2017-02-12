3983a4deb7a87ecc75b987634f6b6722b79a0f2b
Andrew Menning <menning23@gmail.com>
2017-02-12 17:58:49 -0500

    Updating Post model

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211230141) do
+ActiveRecord::Schema.define(version: 20170212223656) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
